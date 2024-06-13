2338b6bba9a38b993fb51eed7a896536e67088a4
Mr-xiBel <35763527+Mr-xiBel@users.noreply.github.com>
2024-06-13 10:36:58 +0800

    Update ip.js

--- functions/ip.js
+++ functions/ip.js
@@ -1,18 +1,33 @@
 export async function onRequest(context) {
     const { request, env } = context;
-    const ip = request.headers.get("x-forwarded-for") || request.headers.get("clientIP")
-    const asOrganization = request.cf.asOrganization || ""
-    return Response.json({
-        "ip": ip,
+    let ip = request.headers.get("x-forwarded-for") || 
+             request.headers.get("cf-connecting-ip") ||
+             request.headers.get("clientIP") ||
+             request.headers.get("forwarded")?.split(", ")[0].split("=")[1]; // 解析forwarded头中的IP
+    
+    // 如果forwarded头存在且格式符合预期，提取IP
+    if (!ip && request.headers.get("forwarded")) {
+        const forwardedParts = request.headers.get("forwarded").split("; ");
+        for (const part of forwardedParts) {
+            if (part.startsWith("for=")) {
+                ip = part.substring(4);
+                break;
+            }
+        }
+    }
+
+    const asOrganization = request.cf.asOrganization || "";
+    
+    return new Response(JSON.stringify({
+        "ip": ip || "Unknown",
         "asOrganization": asOrganization
-    }, {
+    }), {
         headers: {
             "content-type": "application/json;charset=UTF-8",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": "true",
             "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Methods": "*",
-            "ip": ip
+            "Access-Control-Allow-Methods": "*"
         },
     });
 }
